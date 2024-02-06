64179e70a03c768cee42a361da390fb33e2dad3b
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2024-02-05 22:00:36 -0300

    projeto buscador de ceps

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import './App.css'
+import api from './server/api'
+
+function App() {
+  const [count, setCount] = useState('')
+  const [cep, setCep] = useState({})
+
+ async function teste() {
+    if(count === '') {
+      alert('preencha os dados.')
+      return;
+    }
+
+    try{
+     const response = await api.get(`${count}/json`)
+     setCep(response.data)
+     setCount('')
+     
+    }catch{
+     alert('cep invalido.')
+     setCount('')
+    }
+
+  }
+
+  return (
+    <>
+      <div className='container'>
+        <h1 className='titulo'>Buscador de CEP</h1>
+
+        <div className='input'>
+          <input type="text" name="" id="" placeholder='digite seu cep' value={count}
+          onChange={(e) => setCount(e.target.value)} />
+
+          <button className='botao' onClick={teste}> 🔎</button>
+        </div>
+
+        {Object.keys(cep).length > 0 && (
+          <main className='main'>
+          <h2>Cep: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+        )}
+
+      </div>
+    </>
+  )
+}
+
+export default App
